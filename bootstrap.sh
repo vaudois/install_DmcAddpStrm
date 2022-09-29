35854a46ad2a0be82db98663273417ec7b9829e0
vaudois <info@vaudois.ch>
2022-09-29 03:20:32 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # This scrypt run to install Daemon Coin & Addport & Straum
 #########################################################
 if [ -z "${TAG}" ]; then
-	TAG=v0.3
+	TAG=v0.4
 fi
 
 DIRINSTALL=daemoncoin-addport-stratum
